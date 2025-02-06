926c2db974c890f7680edc017f8a07d34747f645
Andika Bahari Maskar <166102600+AndikaMaskar@users.noreply.github.com>
2025-02-06 18:06:53 +0700

    tugaspertemuan1

---
+++ tugaspertemuan1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java;
+
+/**
+ *
+ * @author Acer Nitro 5
+ */
+public class tugaspertemuan1 {
+    public static void main(String[] args){
+        System.out.println("Hallo World saya Andika Bahari Maskar dan NIM saya 2304076. Saya Senang Belajar Java");
+    }
+}
